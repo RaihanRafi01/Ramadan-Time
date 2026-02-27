c18d51fcf28d251d547b0ab81036740941524e94
mri.rafi <mri.rafi@bitopibd.com>
2026-02-27 09:54:12 +0600

    init

--- lib/main.dart
+++ lib/main.dart
@@ -11,6 +11,7 @@ class CountdownApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Ramadan Countdown',
       theme: ThemeData(
         useMaterial3: true,
